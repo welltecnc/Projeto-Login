f3d222090a7c724fe12aed77a0e7c714f8fe5af8
Wellington Cidade <welltecnic@hotmail.com>
2022-04-23 13:02:36 -0300

    projeto login inicio

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import {Button,Input,Text} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text h2>Entre com seu Login</Text>
+      <Input
+      placeholder="usuario"
+      leftIcon={{type:'font-awesome', name: 'user'}}
+      onChangeText={value => Text(value)}
+      />
+
+      <Input
+      placeholder="Digite sua Senha"
+      leftIcon={{type: 'font-awesome', name:'key'}}
+      onChangeText={value=> setPassword(value)}
+      secureTextEntry={true}
+      />
+      <Button
+       icon={
+         <icon
+         name="check"
+         size={15}
+         color="white"
+      />
+
+       }
+       title="Logar"
+       onPress={()=> entrar}
+      />
+
+
+
+
+
     </View>
   );
 }
@@ -13,7 +42,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffff00',
     alignItems: 'center',
     justifyContent: 'center',
   },
